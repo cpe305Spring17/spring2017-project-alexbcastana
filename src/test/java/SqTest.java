308c79d823751e5ded5910051f1fc7847a8993d3
alexbcastana <alexbcastana@gmail.com>
2017-05-10 15:53:22 -0700

    added first Unit Test

---
+++ src/test/java/SqTest.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Marthxander on 5/10/2017.
+ */
+
+import Board.board.Square;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SqTest
+{
+   @Test
+   public void validateSetters() {
+      Square square = new Square(true, true, true);
+      assertTrue(square.isOccupied());
+      square.setOccupied(false);
+      assertFalse(square.isOccupied());
+   }
+
+}
