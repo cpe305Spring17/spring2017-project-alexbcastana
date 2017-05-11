0416d5be47753b9620493488e89e615bc1864035
alexbcastana <alexbcastana@gmail.com>
2017-05-10 17:49:22 -0700

    swapped spots in travis

--- src/main/java/Board/board/Square.java
+++ src/main/java/Board/board/Square.java
@@ -8,14 +8,14 @@ public class Square {
   private Piece piece;
 
   //Square on the board. 64 squares per board
-  public Square(boolean IsOccupied, boolean isBlack, boolean isKing) {
+  public Square(boolean isOccupied, boolean isBlack, boolean isKing) {
 
     if (isKing) {
       this.piece = new King(this, isBlack);
     } else {
       this.piece = new Pawn(this, isBlack);
     }
-    this.isOccupied = IsOccupied;
+    this.isOccupied = isOccupied;
 
   }
 
