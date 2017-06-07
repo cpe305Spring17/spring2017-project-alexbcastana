75d6b4d7198d56ed079b98059832c2c64ea492e8
alexbcastana <alexbcastana@gmail.com>
2017-06-07 10:34:29 -0700

    finished square and piece tests

--- src/test/java/SqTest.java
+++ src/test/java/SqTest.java
@@ -33,6 +33,7 @@ public class SqTest extends TestCase
       square.setPiece(new Pawn(square, true));
       assertTrue(square.getPiece() instanceof Pawn);
       assertEquals(square.getPiece().getPtValue(), 10);
+      assertEquals(square.getPiece().getColor(), "| y |");
    }
 
    @Test
@@ -43,6 +44,12 @@ public class SqTest extends TestCase
       square = new Square(true, false, false);
       assertFalse(square.kingMe(0));
       assertTrue(square.kingMe(7));
+      square = new Square(true, true, true);
+      assertFalse(square.kingMe(0));
+      assertFalse(square.kingMe(7));
+      square = new Square(true, false, true);
+      assertFalse(square.kingMe(0));
+      assertFalse(square.kingMe(7));
    }
 
    @Test
@@ -54,5 +61,6 @@ public class SqTest extends TestCase
       assertEquals(square.getPiece().getFaction(), "Empty");
       assertNull(square.getPiece().getColor());
       assertEquals(square.getPiece().getPtValue(), 0);
+      assertEquals(square.toString(), "| - |");
    }
 }
